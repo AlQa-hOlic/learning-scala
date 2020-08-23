a72e54c256d3383da7e40de798b1850db8aa8e83
AlQa-hOlic <rohit.k.kesavan@gmail.com>
2020-08-23 16:53:56 +0530

    Learn about scala options and failures/Try

---
+++ src/lectures/fp/failures.scala
@@ -0,0 +1,25 @@
+import scala.util.{Success,Failure,Try}
+
+object Failures extends App {
+  val aSuccess = Success(3)
+  val aFailure = Failure(new RuntimeException("such a failure"))
+
+  println(aSuccess)
+  println(aFailure)
+
+  def unsafeMethod(): String = throw new RuntimeException("No string for you!!!!")
+
+  val potentialFail = Try(unsafeMethod())
+  println(potentialFail)
+
+  // syntactic suger
+  val someOtherFailure = Try {
+    //code that might throw exception
+  }
+
+  println(potentialFail.isSuccess)
+
+  def backupMethod(): String = "Success!!!!"
+  val fallback = Try(unsafeMethod()).orElse(Try(backupMethod()))
+  println(fallback)
+}
---
+++ src/lectures/fp/options.scala
@@ -0,0 +1,50 @@
+import scala.util.Random
+
+object Options extends App {
+  val myFirstOption = Option(4)
+  println(myFirstOption)
+
+  val config: Map[String, String] = Map(
+    "host" -> "172.168.1.1",
+    "port" -> "80"
+  )
+
+  object Connection {
+    val random = new Random(System.nanoTime())
+
+    def apply(host: String, port: String): Option[Connection] = 
+      if (random.nextBoolean()) Some(new Connection)
+      else None
+  }
+
+  class Connection {
+    def connect = "Connected"
+  }
+
+  // val host = config.get("host")
+  // val port = config.get("port")
+  // val connection = host.flatMap(h => port.flatMap(p => Connection(h, p)))
+  // val connectionStatus = connection.map(c => c.connect)
+  // connectionStatus.foreach(println)
+
+  // config.get("host")
+  //   .flatMap(host => config.get("port")
+  //     .flatMap(port => Connection(host, port))
+  //     .map(connection => connection.connect))
+  //     .foreach(println)
+
+  // Use for comprehensions for more readable map/flatmap/filter
+  
+  // Read as...given a host from config,
+  // given a port from config &
+  // given a connection from Connnection
+  // yield connectionStatus
+
+  val connectionStatus = for {
+    host <- config.get("host")
+    port <- config.get("port")
+    connection <- Connection(host, port)
+  } yield connection.connect
+
+  connectionStatus.foreach(println)
+}
