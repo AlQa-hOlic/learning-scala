94046563d6689877a6c4462d9146b898d5a4a22f
AlQa-hOlic <rohit.k.kesavan@gmail.com>
2020-08-25 16:06:40 +0530

    More Pattern matching concepts

--- src/main/scala/lectures/fp/Patterns.scala
+++ src/main/scala/lectures/fp/Patterns.scala
@@ -4,11 +4,12 @@ import scala.util.Try
 
 object Patterns extends App {
 
-  val aNumber = 1
+  val aNumber: Any = 1
 
   val patternResult = aNumber match {
     case 0 => "zero"
     case 1 => "one"
+    case x: String => "match by type"
     case _ => "don't know"
   }
 
@@ -39,4 +40,39 @@ object Patterns extends App {
     case _ => false
   }
 
+  val aStandardList = List(1, 2, 3, 4)
+  val listPatternMatching: Unit = aStandardList match {
+    case List(1, _, _, _) => // constructor extractor is implemented for List (advanced pattern matching)
+    case List(1, _*) => // var args patterns - list of arbitrary length of params
+    case 1 :: List(_) => // infix pattern ? don't know what this is...
+    case List(1, 2, 3) :+ 42 => // infix pattern
+    case List(1, _, _, _) | List(_, _, _, 4) => // starts with 1 or ends with 4
+    case List(x, _*) if x == 1 => // if the head is 1 (if guards)
+  }
+
+  try {
+    // catch blocks are actually pattern matches
+  } catch {
+    case _: RuntimeException => "runtime"
+    case _ => "dunno what error"
+  }
+
+  val list = List(1, 2, 3, 4)
+  val evenNumbers = for {
+    x <- list if x % 2 == 0 // Patterns here toooo!!!
+  } yield 10 * x
+
+  val tupleList = List((1, 2), (3, 4))
+  val tupleProd = for {
+    (x, y) <- tupleList
+  } yield x * y
+
+  println(tupleProd)
+
+  // Partial functions are based on pattern match..its like x => x match { //pattern }
+  val mappedList = list.map {
+    case v if v % 2 == 0 => "is even"
+    case _ => "is odd"
+  }
+  println(mappedList)
 }
